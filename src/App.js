bebeec5577f8d11d2b4fbccc0c67d86af64477e8
coderFeedForwardAlg <gaspersscottma@gmail.com>
2024-10-27 22:32:20 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [title, setTitle] = useState("");
+
+  const updateTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const upload = async (event) => {
+    event.preventDefault();
+    const data = {
+      "title": "job_title"
+    }
+
+    try {
+      
+      const reqest = new Request("https://docapi.team-stingray.com/insret_test",  {
+        
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        mode: 'cors'
+      })
+      const res = await fetch(reqest);
+      if (!res.ok) {
+        throw new Error(`Response status: ${res.status}`);
+      }
+      const json = await res.json();
+      console.log(json);
+    }catch (error) {
+      console.error(error.message);
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello world
+      <input type="text" value={title} onChange={updateTitle} />
+      <p>You entered: {title}</p>
+      <button onClick={upload}> upload data </button>
     </div>
   );
 }
