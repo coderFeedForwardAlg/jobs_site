87c830af42e71e74f20437fc96626653e4fb621c
coderFeedForwardAlg <gaspersscottma@gmail.com>
2024-11-02 10:34:46 -0400

    init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 function App() {
   const [title, setTitle] = useState("");
 
+  const [retern, setReturn] = useState("");
+
   const updateTitle = (event) => {
     setTitle(event.target.value);
   };
@@ -9,7 +11,7 @@ function App() {
   const upload = async (event) => {
     event.preventDefault();
     const data = {
-      "title": "job_title"
+      "title": title
     }
 
     try {
@@ -33,6 +35,33 @@ function App() {
       console.error(error.message);
     }
   }
+
+  const get_data = async (event) => {
+    event.preventDefault();
+    const data = {
+      "title": title
+    }
+
+    try {
+      
+      const reqest = new Request("https://docapi.team-stingray.com/recipes",  {
+        
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        mode: 'cors'
+      })
+      const res = await fetch(reqest);
+      if (!res.ok) {
+        throw new Error(`Response status: ${res.status}`);
+      }
+      const json = await res.json();
+      setReturn(json[json.length - 1].title);
+    }catch (error) {
+      console.error(error.message);
+    }
+  }
   
   return (
     <div>
@@ -40,6 +69,9 @@ function App() {
       <input type="text" value={title} onChange={updateTitle} />
       <p>You entered: {title}</p>
       <button onClick={upload}> upload data </button>
+      <br/>
+      <button onClick={get_data}> get the data </button>
+      {retern}
     </div>
   );
 }
